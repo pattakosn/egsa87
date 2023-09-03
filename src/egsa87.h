d7999fd5de2f769f3ad1dce2ed7d9fa1132e131b
N. Pattakos <pattakosn@fastmail.com>
2023-09-03 23:43:19 +0200

    mods based on my taste

--- src/egsa87.h
+++ src/egsa87.h
@@ -1,16 +1,8 @@
 #ifndef _EGSA87_H
 #define _EGSA87_H
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 void wgs84egsa87(double philam[2], double xy[2]);
 void egsa87wgs84(double xy[2], double philam[2]);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif
 
--- src/egsa87_test.cpp
+++ src/egsa87_test.cpp
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <math.h>
+#include <cstdio>
+#include <cmath>
 #include "egsa87.h"
 
 int main(int argc, char *argv[])
